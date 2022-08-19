7d51340a6bca22652ee10959f19d5a3d4ba4907f
ThomasDesc <97127786+ThomasDesc@users.noreply.github.com>
2022-08-19 16:00:25 -0400

    Update job_template.sh

--- job_template.sh
+++ job_template.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4GB
-#SBATCH --time=3:00:00
+#SBATCH --time=2:00:00
 #SBATCH --account=rrg-najmanov
 source /home/thomasd/ENV/bin/activate
 
